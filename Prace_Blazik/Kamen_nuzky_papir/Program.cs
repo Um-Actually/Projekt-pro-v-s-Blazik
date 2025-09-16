7e02ed6d76090c3350a1f47478dbd337ef3476ad
Jakub Blažík <blazikj24@spstrutnov.cz>
2025-09-16 11:35:12 +0200

    prvni pokus

--- Prace_Blazik/Kamen_nuzky_papir/Program.cs
+++ Prace_Blazik/Kamen_nuzky_papir/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            string[] moznosti= { "kamen", "nuzky", "papir" };   
+            Random rnd = new Random();
+            int index = rnd.Next(moznosti.Length);
+            string bot = moznosti[index].ToLower();
+            Console.WriteLine(bot);
+            Console.WriteLine("Co chcete hrát (kamen,nuzky,papir)?");
+            string hrac = Console.ReadLine()?.ToLower()??"";
+            
+            if (hrac == bot)
+            {
+                Console.WriteLine("Remiza");
+            }
+            else if ((hrac == "kamen" && bot == "nuzky") || (hrac == "nuzky" && bot == "papir") || (hrac == "papir" && bot == "kamen"))
+            {
+                Console.WriteLine("Vyhral jsi");
+            }
+            else
+            {
+                Console.WriteLine("Prohral jsi");
+            }
     }
 }
