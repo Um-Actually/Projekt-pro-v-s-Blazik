7fb1d89d3f602825a611dad4998f071be4b58812
Jakub Blažík <blazikj24@spstrutnov.cz>
2025-09-16 17:04:44 +0200

    dodělání kámen nůžky papír na 3

--- Prace_Blazik/Kamen_nuzky_papir/Program.cs
+++ Prace_Blazik/Kamen_nuzky_papir/Program.cs
@@ -4,25 +4,37 @@
     {
         static void Main(string[] args)
         {
-            string[] moznosti= { "kamen", "nuzky", "papir" };   
+            string[] moznosti = { "kamen", "nuzky", "papir" };
             Random rnd = new Random();
-            int index = rnd.Next(moznosti.Length);
-            string bot = moznosti[index].ToLower();
-            Console.WriteLine(bot);
-            Console.WriteLine("Co chcete hrát (kamen,nuzky,papir)?");
-            string hrac = Console.ReadLine()?.ToLower()??"";
-            
-            if (hrac == bot)
+            int hracBody = 0;
+            int botBody = 0;
+
+            while (hracBody<3 && botBody <3)
             {
-                Console.WriteLine("Remiza");
+
+                int index = rnd.Next(moznosti.Length);
+                string bot = moznosti[index].ToLower();
+                Console.WriteLine("Co chcete hrát (kamen,nuzky,papir)?");
+                string hrac = Console.ReadLine()?.ToLower() ?? "";
+
+                if (hrac == bot)
+                {
+                    Console.WriteLine("Remiza");
+                }
+                else if ((hrac == "kamen" && bot == "nuzky") || (hrac == "nuzky" && bot == "papir") || (hrac == "papir" && bot == "kamen"))
+                {
+                    Console.WriteLine("Vyhral jsi");
+                    hracBody+=1;
+                    Console.WriteLine("Skore bot: " + botBody + " Skore hrac:" + hracBody);
+                }
+                else
+                {
+                    Console.WriteLine("Prohral jsi");
+                    botBody += 1;
+                    Console.WriteLine("Skore bot: " + botBody + " Skore hrac: " + hracBody);
+                }
             }
-            else if ((hrac == "kamen" && bot == "nuzky") || (hrac == "nuzky" && bot == "papir") || (hrac == "papir" && bot == "kamen"))
-            {
-                Console.WriteLine("Vyhral jsi");
-            }
-            else
-            {
-                Console.WriteLine("Prohral jsi");
-            }
+        }
     }
+
 }
