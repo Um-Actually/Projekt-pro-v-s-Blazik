21153a2b24358fc9dfa2f708c592479e5a60c9c1
Jakub Blažík <blazikj24@spstrutnov.cz>
2025-09-29 18:32:39 +0200

    dokončení základní logiky šibenice
    chybí dodělat možnost přidat slovo a taky načítat slovo z externího souboru

--- Prace_Blazik/sibenice/Program.cs
+++ Prace_Blazik/sibenice/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace sibenice
+﻿using System.Xml.Xsl;
+
+namespace sibenice
 {
     internal class Program
     {
@@ -8,27 +10,39 @@
             List<string> slova = new List<string> { "Pepa"};
             string slovo = slova[rnd.Next(slova.Count)].ToLower();
             char[] pismena = slovo.ToCharArray();
+            char[] zkryte_slovo= new char[slovo.Length];
+
+            for (int i = 0; i < slovo.Length; i++)
+            {
+                zkryte_slovo[i] = '_';
+            }
 
             int pokusy = 6;
             while (pokusy > 0)
             {
                 Console.WriteLine("Napište písmeno co chcete hrát");
-                string input = Console.ReadLine().ToLower();
-                char hrac= input[0].ToCharArray();
+                string input = Console.ReadLine().ToLower(); ;
 
-                for (int i = 0; i<pismena.Length;i++)
-                { 
-                if (hrac[0] == pismena[i])
+                for (int i = 0; i < pismena.Length; i++)
+                {
+                    if (input[0] == pismena[i])
                     {
-                        pismena[i] = hrac[0];
-                        Console.WriteLine(pismena[i]);
+                        zkryte_slovo[i] = input[0];
                     }
-                else
-                    {
-                        Console.WriteLine("_");
-                    }
+
+
                 }
-                
+                Console.WriteLine(zkryte_slovo);
+                if (!slovo.Contains(input[0]))
+                {
+                    pokusy--;
+                    Console.WriteLine($"Špatně, zbývá {pokusy} pokusů");
+                }
+                if (!zkryte_slovo.Contains('_'))
+                {
+                    Console.WriteLine("Vyhrál jsi");
+                    pokusy = -125000165;
+                }
             }
         }
     }
